c53665375358d966fd1961e04aca86f754f1972a
Rohit <rohitrohan25420@gmail.com>
2022-06-16 12:12:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios'
+import styled from "styled-components";
+import CityComponent from './components/CityComponent';
+import WeatherInfoComponent from './components/WeatherInfoComponent';
+
+const API_KEY = "d215b17398a2606edab5911f93361c47";
+
+
+export const WeatherIcons = {
+  "01d": "/icons/sunny.svg",
+  "01n": "/icons/night.svg",
+  "02d": "/icons/day.svg",
+  "02n": "/icons/cloudy-night.svg",
+  "03d": "/icons/cloudy.svg",
+  "03n": "/icons/cloudy.svg",
+  "04d": "/icons/perfect-day.svg",
+  "04n": "/icons/cloudy-night.svg",
+  "09d": "/icons/rain.svg",
+  "09n": "/icons/rain-night.svg",
+  "10d": "/icons/rain.svg",
+  "10n": "/icons/rain-night.svg",
+  "11d": "/icons/storm.svg",
+  "11n": "/icons/storm.svg",
+};
+
+
+const Container =styled.div`
+display:flex;
+flex-direction: column;
+margin:auto;
+align-items:center;
+box-shadow: 0 3px 6px 0 #555;
+padding:20px 10px;
+border-radious:4px;
+width:380px;
+background: white;
+font-family: 'Montserrat'
+`
+const AppLabel =styled.span`
+color:black;
+font-size: 18px;
+font-weight:bold;
+`
 
 function App() {
+  const [city,setCity] = useState();
+  const [weather, setWeather] = useState();
+
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    const response = 
+    await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+    setWeather(response.data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+     <AppLabel>React Weather App</AppLabel> 
+     {weather ? (
+       <WeatherInfoComponent weather={weather} />
+       ) : (
+     <CityComponent setCity={setCity} fetchWeather={fetchWeather} />
+       )}
+    </Container>
   );
 }
 
---
+++ src/components/WeatherInfoComponent.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import styled from 'styled-components'
+import {WeatherIcons} from "../App";
+
+export const WeatherInfoIcons = {
+  sunset: "/icons/temp.svg",
+  sunrise: "/icons/temp.svg",
+  humidity: "/icons/humidity.svg",
+  wind: "/icons/wind.svg",
+  pressure: "/icons/pressure.svg",
+}
+
+const WeatherCondition = styled.div`
+display:flex;
+flex-direction:row;
+align-items:center;
+width:100%;
+justify-content:space-between;
+margin: 30px auto;
+`;
+const Condition = styled.span`
+margin:20px auto;
+font-size:14px;
+& span {
+    font-size:28px;
+}
+`;
+const WeatherLogo = styled.img`
+width: 100px;
+height: 100px;
+margin: 5px auto;
+`;
+const Location = styled.span`
+font-size:28px;
+font-weight:bold;
+
+`;
+const WeatherInfoLabel = styled.span`
+font-size:14px;
+font-weight:bold;
+margin:20px 25px 10px;
+text-align:start;
+width:100%;
+`;
+const WeatherInfoContainer = styled.div`
+display: flex;
+width: 90%;
+flex-direction: row;
+justify-content: space-evenly;
+align-items: center;
+flex-wrap:wrap;
+`;
+const InfoContainer = styled.div`
+display:flex;
+margin: 5px 10px;
+flex-direction:row;
+justify-content:space-evenly;
+align-items:center;
+`;
+const InfoIcon = styled.img`
+  width: 36px;
+  height: 36px;
+`;
+
+const InfoLabel = styled.span`
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+  margin: 15px;
+  & span {
+    font-size: 12px;
+    text-transform: capitalize;
+  }
+`;
+const WeatherInfoComponents = (props) => {
+  const { name, value } = props
+  return (
+    <InfoContainer>
+      <InfoIcon src={WeatherInfoIcons[name]} />
+      <InfoLabel>
+        {value}
+        <span>{name}</span>
+      </InfoLabel>
+    </InfoContainer>
+  )
+}
+
+const WeatherInfoComponent = (props) => {
+  const { weather } = props;
+  const isDay = weather?.weather[0].icon?.includes('d')
+  const getTime = (timeStamp) => {
+    return `${new Date(timeStamp * 1000).getHours()} : ${new Date(timeStamp * 1000).getMinutes()}`
+  }
+  return (
+    <>
+      <WeatherCondition>
+        <Condition>
+          <span>{`${Math.floor(weather?.main?.temp - 273)}°C`}</span>
+          {`  |  ${weather?.weather[0].description}`}
+        </Condition>
+        <WeatherLogo src={WeatherIcons[weather?.weather[0].icon]}/>
+      </WeatherCondition>
+      <Location>{`${weather?.name}, ${weather?.sys?.country}`}</Location>
+      <WeatherInfoLabel>Weather Info</WeatherInfoLabel>
+      <WeatherInfoContainer>
+        <WeatherInfoComponents name={isDay ? "sunset" : "sunrise"}
+          value={`${getTime(weather?.sys[isDay ? "sunset" : "sunrise"])}`} />
+        <WeatherInfoComponents name='humidity' value={weather?.main?.humidity} />
+        <WeatherInfoComponents name='wind' value={weather?.wind?.speed} />
+        <WeatherInfoComponents name='pressure' value={weather?.main?.pressure} />
+      </WeatherInfoContainer>
+    </>
+  )
+}
+
+export default WeatherInfoComponent
